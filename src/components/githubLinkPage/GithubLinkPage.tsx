ef50e4cabee13747bc567ad5c5eee426923284f4
najwalahly <najwa.lahly@gmail.com>
2023-10-25 18:24:42 +0200

    add return to home page

--- src/components/githubLinkPage/GithubLinkPage.tsx
+++ src/components/githubLinkPage/GithubLinkPage.tsx
@@ -1,8 +1,20 @@
+import { NavLink } from "react-router-dom";
 export default function GithubLinkPage() {
-    return(
-        <div>
-            <h1>To Visit my Github</h1>
-            <a href="">Click here</a>
-        </div>
-    )
+  return (
+    <div className="container">
+      <h1>To Visit my Github</h1>
+      <p>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/NajwaLahly"
+        >
+          Click here
+        </a>
+      </p>
+      <p>
+        <NavLink to={"/"}>Back to home page</NavLink>
+      </p>
+    </div>
+  );
 }
